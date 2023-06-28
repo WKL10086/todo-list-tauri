34b7c592736a8033e4bc488fe679f513c7dc2c10
Peter Wong <peter.wong@deepcodify.com>
2023-06-28 11:40:54 +0800

    Add todo list item

---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Todo } from "src/type/todo.type";
+
+  export let item: Todo;
+
+  let id = item.id.toString();
+</script>
+
+<div>
+  <h2>{item.title}</h2>
+  <label for={id} />
+  <input type="checkbox" {id} name={id} value={item.completed} />
+</div>
