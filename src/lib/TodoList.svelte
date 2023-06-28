372a63296bf44aceebc9869d14b4ea4e310a0d6e
Peter Wong <peter.wong@deepcodify.com>
2023-06-28 11:42:07 +0800

    Add todo list

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import TodoItem from "./TodoItem.svelte";
+  import type { Todo } from "src/type/todo.type";
+
+  let todoList: Todo[] = [];
+
+  async function getTodoList() {
+    todoList = await invoke("getTodoList");
+  }
+
+  getTodoList();
+</script>
+
+<div>
+  <h1>Todo List</h1>
+  {#each todoList as item (item.id)}
+    <TodoItem {item} />
+  {/each}
+</div>
