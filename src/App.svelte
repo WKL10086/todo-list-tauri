69312a606fe360573180f01626534a9bb669cb27
Peter Wong <peter.wong@deepcodify.com>
2023-07-04 18:06:13 +0800

    Add pico to app svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import TodoList from "./lib/TodoList.svelte";
+  import "@picocss/pico";
 </script>
 
 <main class="container">
@@ -7,5 +8,13 @@
 
   <div class="row">
     <TodoList />
+    <form>
+      <input type="text" placeholder="Text" aria-label="Text" />
+      <input type="email" placeholder="Email" aria-label="Email" />
+      <input type="number" placeholder="Number" aria-label="Number" />
+      <input type="password" placeholder="Password" aria-label="Password" />
+      <input type="tel" placeholder="Tel" aria-label="Tel" />
+      <input type="url" placeholder="Url" aria-label="Url" />
+    </form>
   </div>
 </main>
