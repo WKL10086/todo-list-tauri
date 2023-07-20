8f59fc0c044f64b252471a08666d8ab393c6f261
Peter Wong <peter.wong@deepcodify.com>
2023-07-20 18:05:18 +0800

    Add comment for importing pico

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import TodoList from "./lib/components/todo/TodoList.svelte";
+  // it would affect the whole app no matter where it is imported
   import "@picocss/pico";
+
+  import TodoList from "./lib/components/todo/TodoList.svelte";
 </script>
 
 <main class="container">
@@ -8,13 +10,5 @@
 
   <div class="row">
     <TodoList />
-    <form>
-      <input type="text" placeholder="Text" aria-label="Text" />
-      <input type="email" placeholder="Email" aria-label="Email" />
-      <input type="number" placeholder="Number" aria-label="Number" />
-      <input type="password" placeholder="Password" aria-label="Password" />
-      <input type="tel" placeholder="Tel" aria-label="Tel" />
-      <input type="url" placeholder="Url" aria-label="Url" />
-    </form>
   </div>
 </main>
